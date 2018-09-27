bb4ef4a2ce7a9550b25ac612fb9cd5097191db0c
micplo <michael.ploj@sinnerschrader.com>
2018-09-27 23:30:37 +0200

    main styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,14 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design & code react apps</h1>
-    <p>This is a test course for learning React code. This is based on Design & Code course.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design & code react apps</h1>
+        <p>This is a test course for learning React code. This is based on Design & Code course.</p>
+        <Link to="/page-2/">Leading somewhere</Link>
+      </div>
+    </div>
+    
   </Layout>
 )
 
